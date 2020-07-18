1e0337c885674b4781cc5ad3ad011d5c5b23def6
Brandon Nichols <brandonaronnichols@gamil.com>
2020-07-17 22:05:03 -0500

    completed ex2.py

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,29 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
+    ticket_cache = {}
+    route = []
+
+    for ticket in tickets:
+        if ticket.source not in ticket_cache:
+            ticket_cache[ticket.source] = ticket.destination
+
+    route.append(ticket_cache["NONE"])
+
+    current_destination = route[0]
+
+    while current_destination != "NONE":
+        current_destination = ticket_cache[current_destination]
+        route.append(current_destination)
+    
     # Your code here
 
     return route
+
+# ticket_1 = Ticket("NONE", "PDX")
+# ticket_2 = Ticket("PDX", "DCA")
+# ticket_3 = Ticket("DCA", "NONE")
+
+# tickets = [ticket_1, ticket_2, ticket_3]
+
+# reconstruct_trip(tickets, 3)
